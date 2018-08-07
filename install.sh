0557db7190192414da5ca8deaa4e5380bf40b878
Tom Newby <me@tomnewby.net>
2018-08-07 20:40:44 +1000

    Updated with fewer preferred programs

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+echo "We're going to ask for your password just once, so feel free to go do something else after this, hopefully it should be done then 🎉"
+
 # Ask for the administrator password upfront
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# To install gcc, make etc. we need to include Xcode command line dev tools
+# it's a stupid requirement but #macintoshlyf
 echo 'Installing Xcode Command Line Tools...'
 xcode-select --install
 sleep 1
@@ -18,48 +22,39 @@ osascript <<EOD
   end tell
 EOD
 
+# Setup homebrew, your main package manager
 echo 'Installing Homebrew...'
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Cask lets you install binary apps instead of source code, so we install a lot with cask
 echo 'Installing Homebrew Cask...'
 brew install caskroom/cask/brew-cask
 
 echo 'Running brew update...'
 brew update
 
-echo 'Installing git...'
-brew install git
+# This install specific formulas from homebrew
+formulas=(git tmux hub awscli)
 
-echo 'Installing spectacle...'
-brew cask install spectacle
+for formula in ${formulas[*]}
+do
+	printf "Installing %s\n" $formula
+	brew install $formula
+done
 
-echo 'Installing Google Chrome...'
-brew cask install google-chrome
+# This installs specific apps via Homebrew Cask
+casks=(spectacle google-chrome slack spotify jetbrains-toolbox virtualbox vagrant)
+for cask in ${casks[*]}
+do
+	printf "Installing %s\n" $cask
+	brew cask install $cask
+done
 
-echo 'Installing Boom'
-brew cask install boom
-
-echo 'Installing Sketch'
-brew cask install sketch
-
-echo 'Install Sketch Toolbox'
-brew cask install sketch-toolbox
-
+# Start fish shell config
 echo 'Installing Fish shell'
 brew install fish
 
 echo 'Setting Fish to be default shell'
 echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
-
-echo 'Installing Slack'
-brew cask install slack
-
-echo 'Installing tmux'
-brew install tmux
-
-echo 'Installing Hammerspoon'
-./hammerspoon/install.sh
-
-echo 'Installing Spotify'
-brew cask install spotify
+# End fish shell config
