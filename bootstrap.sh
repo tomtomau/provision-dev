246ea11a13c713a24cc780f31547181059b58f9d
Tom Newby <me@tomnewby.net>
2021-11-22 12:41:14 +1000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,30 +2,14 @@
 
 set -eux
 
-echo "We're going to ask for your password just once, so feel free to go do something else after this, hopefully it should be done then 🎉"
-
 # Update to Rosetta 2
 /usr/sbin/softwareupdate --install-rosetta --agree-to-license
-
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # To install gcc, make etc. we need to include Xcode command line dev tools
 # it's a stupid requirement but #macintoshlyf
 echo 'Installing Xcode Command Line Tools...'
 xcode-select --install
-sleep 1
-osascript <<EOD
-  tell application "System Events"
-    tell process "Install Command Line Developer Tools"
-      keystroke return
-      click button "Agree" of window "License Agreement"
-    end tell
-  end tell
-EOD
+softwareupdate --all --install --force
 
 mkdir -p ~/src/tomtomau/
 
